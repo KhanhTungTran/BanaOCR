0103c3ffa8abd57a3d813993d298b8d755d78108
Tùng Trần UT <tung.tran.1805@hcmut.edu.vn>
2021-03-09 11:53:17 +0700

    Refactor run.py

--- correction.py
+++ correction.py
@@ -1,5 +1,4 @@
 import os
-import pprint
 
 inputDir = 'output'
 resultDir = 'result'
--- run.py
+++ run.py
@@ -1,34 +1,28 @@
-import os
 import pytesseract
 import cv2
-from skimage import io
+# from skimage import io
 import numpy as np
 from imutils import paths
 
-inputDir = 'input'
-outputDir = 'output'
-imageName = list(filter(lambda file: file[-3:] == 'png', os.listdir(inputDir)))
-paths.list_images()
+inputDir = 'input/'
+outputDir = 'output/'
+# imageName = list(filter(lambda file: file[-3:] == 'png', os.listdir(inputDir)))
+imageName = list(paths.list_images(inputDir))
 
-def loadImage(img_file):
-    img = io.imread(img_file)           # RGB order
-    if img.shape[0] == 2: img = img[0]
-    if len(img.shape) == 2 : img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    if img.shape[2] == 4:   img = img[:,:,:3]
-    img = np.array(img[:,:,::-1])
-
+def loadImage(imgPath):
+    img = cv2.imread(imgPath)
+    # Convert to gray scale
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
 
-for image in imageName:
-    img = io.imread(inputDir + '/' + image)
-    print(img.shape)
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # img = loadImage(inputsDir + '/' + image)
-    cv2.imshow("original", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+for imgPath in imageName:
+    print(imgPath)
+    img = loadImage(imgPath)
+    # cv2.imshow("original", img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     custom_oem_psm_config = '--psm 6'
 
-    with open(outputDir + '/' + image[:-3] + 'txt', 'w', encoding='utf-8') as f:
+    with open(outputDir + imgPath[imgPath.rfind('/') + 1:-3] + 'txt', 'w', encoding='utf-8') as f:
         f.write(pytesseract.image_to_string(img, lang='vie', config=custom_oem_psm_config))
